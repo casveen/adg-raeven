52f92939833720c2c1e256431bfa5293129f5211
fnugus <christiansveen@hotmail.com>
2025-02-14 19:41:31 +0100

    fixup! (particles): spore-puff

--- src/gameplay/src/puff.rs
+++ src/gameplay/src/puff.rs
@@ -91,54 +91,6 @@ fn create_effect(mesh: Handle<Mesh>, effects: &mut Assets<EffectAsset>) -> Handl
             .expr(),
     );
 
-    // Make the particles move backwards at a constant speed.
-    /*let init_velocity = SetAttributeModifier::new(
-        Attribute::VELOCITY,
-    
-        writer.lit(vec3(0.0, 0.0, -20.0)).expr(),
-    );*/
-
-    /*let velocity = SetAttributeModifier::new(
-        Attribute::VELOCITY,
-
-        (writer.attr(Attribute::VELOCITY)*(writer.lit(1.0)-writer.attr(Attribute::AGE))
-            //writer.attr(Attribute::VELOCITY).normalized()*(writer.attr(Attribute::F32_0).step(writer.lit(1.5)))   
-            
-
-        
-        )
-        .expr()
-        //(writer.rand(ScalarType::Float)*writer.lit(4.0)+writer.lit(1)).expr(),
-    );*/
-
-    /*let update_speed = SetAttributeModifier::new(
-        Attribute::F32_0,
-        (writer.attr(Attribute::F32_0)+writer.lit(0.01)).expr()
-        //(writer.rand(ScalarType::Float)*writer.lit(4.0)+writer.lit(1)).expr(),
-    );*/
-
-    /*let phase_1 = SetAttributeModifier::new(
-        Attribute::F32_0,
-        (writer.attr(Attribute::AGE)).step(writer.lit(1.5)).expr()
-        //(writer.rand(ScalarType::Float)*writer.lit(4.0)+writer.lit(1)).expr(),
-    );
-    let phase_1_val = SetAttributeModifier::new(
-        Attribute::F32_1,
-        (writer.attr(Attribute::F32_0)).mul(writer.attr(Attribute::AGE)).expr()
-        //(writer.rand(ScalarType::Float)*writer.lit(4.0)+writer.lit(1)).expr(),
-    );
-    let phase_2 = SetAttributeModifier::new(
-        Attribute::F32_2,
-        (writer.lit(1)-writer.attr(Attribute::F32_0)).expr()
-        //(writer.rand(ScalarType::Float)*writer.lit(4.0)+writer.lit(1)).expr(),
-    );
-    let phase_2_val = SetAttributeModifier::new(
-        Attribute::F32_3,
-        (writer.attr(Attribute::F32_2)).mul(writer.lit(1.5)-writer.attr(Attribute::AGE)/writer.lit(1.5)).expr()
-        //(writer.rand(ScalarType::Float)*writer.lit(4.0)+writer.lit(1)).expr(),
-    );*/
-
-
     let phase1 = 1.0;
     let phase05 = 0.2;
 
@@ -157,13 +109,6 @@ fn create_effect(mesh: Handle<Mesh>, effects: &mut Assets<EffectAsset>) -> Handl
         )
         .expr()
     );
-
-    /*fn pocket(t: WriterExpr, from: WriterExpr, to: WriterExpr) -> WriterExpr {
-        let u = t.clone();
-        let v = t.clone();
-
-        (writer.lit(1.0)-u.step(from))-(writer.lit(1.0)-v.step(to))
-    }*/
 
     let velocity = SetAttributeModifier::new(
         Attribute::VELOCITY,
@@ -205,11 +150,6 @@ fn create_effect(mesh: Handle<Mesh>, effects: &mut Assets<EffectAsset>) -> Handl
                 Vec4::new(1.0,1.0,1.0,0.0), 
             )
     };
-        /*Attribute::ALPHA,
-        (writer.lit(1)).expr()*/
-    
-
-
 
     let module = writer.finish();
 
@@ -227,10 +167,6 @@ fn create_effect(mesh: Handle<Mesh>, effects: &mut Assets<EffectAsset>) -> Handl
             .update(update_size)
             .update(acceleration)
             .render(update_alpha)
-            //.update(velocity)
-            //.update(init_accel)
-            //.update(update_size)
-            //.render(render_lambertian)
             .mesh(mesh),
     )
 }
@@ -242,12 +178,8 @@ pub struct PuffPlugin;
 impl Plugin for PuffPlugin {
     
     fn build(&self, app: &mut App) {
-
         app.register_type::<Puffable>()
-        //.add_systems(Update, (add_mushrooms_to_bloomables, update_mushroom_size));
         .add_systems(Startup, setup)
         .add_plugins(HanabiPlugin);
-
-        //.add_systems(Update, setup_scene_once_loaded)
     }
 }

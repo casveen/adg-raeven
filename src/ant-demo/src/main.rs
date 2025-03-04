b1e7ece7a117e9cb474f362919a064af88cd733c
Adrian Stræte <adrian.strate@appear.net>
2025-03-04 17:44:02 +0100

    (blenvy-ant-demo) add ant-demo binary

---
+++ src/ant-demo/src/main.rs
@@ -0,0 +1,26 @@
+use bevy::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use blenvy::{BlueprintInfo, GameWorldTag, HideUntilReady, SpawnBlueprint};
+
+fn main() {
+    App::new()
+        .add_plugins((
+            DefaultPlugins,
+            WorldInspectorPlugin::new(),
+            core::CorePlugin,
+        ))
+        .add_systems(Startup, setup_game)
+        .run();
+}
+
+fn setup_game(mut commands: Commands) {
+    commands.spawn((
+        BlueprintInfo {
+            path: "levels/AntDemo.glb".to_string(),
+            name: core::utils::gameplayscene_loadstatus::LEVEL_NAME.to_string(),
+        },
+        SpawnBlueprint,
+        HideUntilReady,
+        GameWorldTag,
+    ));
+}

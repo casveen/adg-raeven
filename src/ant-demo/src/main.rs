1f27220ac86e53eeeb252e925057079d320237b6
Adrian Stræte <adrian.strate@appear.net>
2025-03-08 10:22:10 +0100

    (spawning-player) update ant-demo code with killing player

--- src/ant-demo/src/main.rs
+++ src/ant-demo/src/main.rs
@@ -1,4 +1,6 @@
-use bevy::prelude::*;
+use core::player::controller::KillPlayerEvent;
+
+use bevy::{input::keyboard::KeyboardInput, prelude::*};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use blenvy::{BlueprintInfo, GameWorldTag, HideUntilReady, SpawnBlueprint};
 
@@ -10,6 +12,7 @@ fn main() {
             core::CorePlugin,
         ))
         .add_systems(Startup, setup_game)
+        .add_systems(Update, process_input)
         .run();
 }
 
@@ -23,4 +26,15 @@ fn setup_game(mut commands: Commands) {
         HideUntilReady,
         GameWorldTag,
     ));
+}
+
+// for testing
+fn process_input(keyboard: Res<ButtonInput<KeyCode>>, mut commands: Commands) {
+    for key in keyboard.get_just_pressed() {
+        if key == &KeyCode::KeyZ {
+            commands.trigger(KillPlayerEvent {
+                description: "KeyCode::KeyZ".to_string(),
+            });
+        }
+    }
 }
